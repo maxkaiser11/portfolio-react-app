1fe3bfbce29eec2f92cbd2868012b6c4947a3433
max kaiser <max@kaiser.family>
2023-06-30 18:27:08 +0200

    added rails

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,6 +9,8 @@ import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
+import Rails from '../assets/rails.png'
+import Postgres from '../assets/postgres.png'
 
 const Skills = () => {
   return (
@@ -42,8 +44,8 @@ const Skills = () => {
             <p className='my-4'>GITHUB</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
-            <p className='my-4'>FIREBASE</p>
+            <img className='w-20 mx-auto' src={Rails} alt="HTML icon" />
+            <p className='my-4'>RUBY ON RAILS</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
