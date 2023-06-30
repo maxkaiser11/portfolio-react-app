7e5c0ca5f391ec07be9e9d1ea4a0fe5514c7c54d
max kaiser <max@kaiser.family>
2023-06-30 18:16:33 +0200

    fixed button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from 'react-scroll'
 
 
 const Home = () => {
@@ -12,11 +13,13 @@ const Home = () => {
           <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
           <p className='text-[#8892b0] py-4 max-w-[700px]'>Im a full stack developer focusing on creating exceptional digital experiences</p>
           <div>
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work
-            <span className='group-hover:rotate-90 duration-300'>
-            <HiArrowNarrowRight className='ml-3 hover:' />
-            </span>
-            </button>
+            <Link to="work" smooth={true} duration={500}>
+              <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work
+                <span className='group-hover:rotate-90 duration-300'>
+                <HiArrowNarrowRight className='ml-3 hover:' />
+                </span>
+              </button>
+            </Link>
           </div>
         </div>
     </div>
